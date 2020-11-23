6ac0b3f64e0ce1ea5ec0393401961fc123166055
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-11-23 12:52:08 -0500

    [UPDATE]Filter by categories

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -1,10 +1,16 @@
 const data = require('../../data');
 const postData = data.postData;
 const uniqueTags = data.uniqueTags;
+const categoryData = data.categoryData;
 const recentPostAmount = 3;
 
 const getHomePage = function(req, res) {
-  res.render ("index", { title: "Just Me", posts: postData, active: "index" });
+  res.render ("index", {
+    title: "Just Me",
+    posts: postData,
+    active: "index",
+    categoryData: categoryData
+  });
 };
 
 const getBlogPost = function({params}, res) {
@@ -15,7 +21,7 @@ const getBlogPost = function({params}, res) {
     post: post,
     uniqueTags: uniqueTags,
     recentPosts: postData.slice(0, recentPostAmount),
-    categoryData: data.categoryData
+    categoryData: categoryData
   });
 };
 
@@ -24,7 +30,7 @@ const get404 = function(req, res) {
     title: "404 - I couldn't find that page.",
     uniqueTags: uniqueTags,
     recentPosts: postData.slice(0, recentPostAmount),
-    categoryData: data.categoryData
+    categoryData: categoryData
   });
 }
 
@@ -33,11 +39,34 @@ const redirect404 = function(req, res) {
 }
 
 const getAbout = function(req, res){
-  res.render('about', { title: 'About Me', active: "about" });
+  res.render('about', {
+    title: 'About Me',
+    active: "about",
+    categoryData: categoryData
+  });
 }
 
 const getContact = function(req, res){
-  res.render('contact', { title: 'Contact Me', active: "contact" });
+  res.render('contact', {
+    title: 'Contact Me',
+    active: "contact",
+    categoryData: categoryData
+  });
+}
+
+
+const getFilteredList = function({query}, res, next) {
+  let filteredPosts = postData.filter((val) => {
+      return val.category == query.category || val.tags.includes(query.tag);
+  });
+
+  //const templateData = 
+  res.render('filter', {
+    title: "Just Me - Filtered",
+    active: query.category,
+    posts: filteredPosts,
+    categoryData: data.categoryData
+});
 }
 
 module.exports = {
@@ -46,5 +75,6 @@ module.exports = {
   get404,
   redirect404,
   getAbout,
-  getContact
+  getContact,
+  getFilteredList
 };
